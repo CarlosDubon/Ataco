394d75293fa71fd40e59e7600e2226aa3d8e902e
CarlosDubon <carlosminerodubon@gmail.com>
2017-10-11 13:06:52 -0600

    boton reproducir conpleto

--- js/player.js
+++ js/player.js
@@ -7,9 +7,15 @@ function iniciar(){
 function reproducir(){
     if(!medio.paused && !medio.ended){
         medio.pause();
-        $('#medio').removeClass('play-buttom');
+        $('i[name="pause"]').after('<i name="play" class="fa fa-play" aria-hidden="true"></i><span name="play">Reproducir</span>');
+         $('i[name="pause"]').remove();
+        $('span[name="pause"]').remove();
+        
     }else{
-        medio.play();   
+        medio.play();
+        $('i[name="play"]').after('<i name="pause" class="fa fa-pause" aria-hidden="true"></i><span name="pause">Pausa</span>');
+         $('i[name="play"]').remove();
+        $('span[name="play"]').remove();
     }
     
 }
