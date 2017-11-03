19e3c1939608ae7ab5b9adc3fb9e6a3d3642b2ce
Kathgo97 <00011616@uca.edu.sv>
2017-11-03 15:46:09 -0600

    poli

---
+++ php/divpolitica.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <?php
+    include '../html/head.html';
+    ?>
+    <title>Orografia de Ataco</title>
+</head>
+<body>
+    <header>
+    <?php
+       include '../html/header.html';
+    ?>
+    </header>
+    <section>
+        
+    </section>
+    <footer>
+        <?php
+          include '../html/Topscroller-dark.html';
+        ?>
+    </footer>
+    <!--Modals-->
+</body>
+</html>
