864dfd8b7ffa1047390bd02c338b1c7159aa857d
CarlosDubon <carlosminerodubon@gmail.com>
2017-11-03 13:56:24 -0600

    gobierno local

---
+++ php/gobierno-local.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?php
+        include '../html/head.html';
+    ?>
+    <title>Gobierno Local</title>
+</head>
+<body>
+   <header>
+       <?php
+            include '../html/header.html';
+       ?>
+   </header>
+   <section>
+       <div class="container">
+           <h1>Gobierno Local</h1>
+           <div class="row">
+               <div class="col-lg-4">
+                   <a href="#" class="thumbnail">
+                      <img class="miniaruta" src="../img/Img8.jpg" alt="...">
+                    </a>
+               </div>
+               <div class="col-lg-8">
+                  <h2>Lorem</h2>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias quisquam cum ipsam veritatis dicta repellendus quae odio accusamus cumque similique perferendis eligendi saepe praesentium officiis ratione unde, possimus numquam cupiditate! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus rem deserunt quidem, voluptates optio maxime. Temporibus dolorem voluptate dicta, repellat quisquam, laborum vero aperiam, tenetur quos reprehenderit, animi voluptates! Omnis.</p>
+               </div>
+           </div>
+       </div>
+   </section>
+    
+</body>
+</html>
