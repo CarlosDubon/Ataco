c83d87d3bb6166edc737affa17803eda4634aa7c
Kathgo97 <00011616@uca.edu.sv>
2017-11-04 12:16:41 -0600

    oro

--- php/orografia.php
+++ php/orografia.php
@@ -14,6 +14,23 @@
     ?>
     </header>
     <section>
+        <div class="container"> 
+              <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <h1><p align="center"> OROGRAFIA DE CONCEPCION DE ATACO</p></h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p> 
+                </div>
+                 <div class="col-lg-12 col-md-12">
+
+                    <img class="miniaruta" src="../img/relieve.jpg" alt="...">
+                </div>
+                 <div class="col-lg-12 col-md-12">
+               
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p> 
+                </div>
+   
+            </div>
+        </div>
         
     </section>
     <footer>
