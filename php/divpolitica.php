479310b442b3ef7fac9a4a023c845f4d4e2c8305
kilmarCabrera <kilmarhdz@gmail.com>
2017-11-05 11:47:50 -0600

    listo

--- php/divpolitica.php
+++ php/divpolitica.php
@@ -5,6 +5,10 @@
     <?php
     include '../html/head.html';
     ?>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <title>Division Politica</title>
 </head>
 <body>
@@ -16,65 +20,60 @@
     <section>
        <div class="container">
             <h1 style="display: inline-block">Division Politica</h1>
+            
             <div class="row">
                 <div class="col-lg-8 col-md-12">
-                    <h1>Cantones</h1>
+                    <h1>Su estructura</h1>
                     <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, fuga quibusdam illo distinctio omnis voluptatum alias blanditiis ratione maiores earum ex aspernatur commodi maxime, veritatis. Ipsum, ab optio? Accusamus, distinctio!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, aliquam, molestias hic iure accusantium sint ex reiciendis, non consectetur beatae sit. Necessitatibus nesciunt libero accusantium? Quibusdam aperiam enim necessitatibus sit. </p>
                 </div>
                 
-                <div class="col-lg-8 col-md-12">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt nisi facilis doloremque! Doloribus omnis, quaerat temporibus odio quo quasi voluptas dolorem. Sunt dolorum nostrum ab velit alias pariatur, libero porro?</p>
+                <div class="col-md-12">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt nisi facilis doloremque! Doloribus omnis, quaerat temporibus odio quo quasi voluptas dolorem. Sunt dolorum nostrum ab velit alias pariatur, libero porro?</p>
                 </div>
             </div>
             
             <hr>
             <div class="row">
                 <div class="col-lg-8 col-md-12">
-                    <h1>Lorem</h1>
+                    <h1>Sus cantones</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-8 col-md-12">
-                    <h1>Lorem</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p>
+                <div class="col-md-12">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p>
                 </div>
             </div>
-            <br>
-            <div class="col-md-12">
-                    <div id="carousel-1" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
-                            <li data-target="#carousel-1" data-slide-to="1"></li>
-                            <li data-target="#carousel-1" data-slide-to="2"></li>
-                        </ol>
-
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="../img/Img1.jpg" class="d-block w-100">
-                                <div class="carousel-caption">
-                                    <h3>Este es un Slide #1</h3>
-                                    <p>Hola mundo</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="../img/Img8.jpg" class="d-block w-100">
-                                <div class="carousel-caption">
-                                    <h3>Este es un Slide #2</h3>
-                                    <p>Hola mundo</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="../img/Img9.jpg" class="d-block w-100">
-                                <div class="carousel-caption">
-                                    <h3>Este es un Slide #3</h3>
-                                    <p>Hola mundo</p>
-                                </div>
-                            </div>
-                        </div>
+            <div class="row">
+                
+                <div id="carouselExampleIndicators" class="carousel slide col-md-12" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="../img/Img8.jpg" alt="First slide">
                     </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="../img/Img4.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="../img/Img9.jpg" alt="Third slide">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
             </div>
-        </div> 
+    </div> 
     </section>
     <footer>
         <?php
