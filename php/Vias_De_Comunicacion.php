f1d0bbd623cb273c94f27eba34946cf7a16b9bce
CarlosDubon <carlosminerodubon@gmail.com>
2017-11-06 16:46:06 -0600

    fsda
    dsaf

--- php/Vias_De_Comunicacion.php
+++ php/Vias_De_Comunicacion.php
@@ -16,8 +16,15 @@
     <div class="container">
       <h1 style="display: inline-block">Vias de comunicacion</h1><button type="button" class="play-buttom"><i name="play" class="fa fa-play" aria-hidden="true"></i><span name="play">Reproducir</span></button>
       <div class="row" >
-        <div class="col-12">
+        <div class="col-md-6">
           <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-md-6">
+            <div class="padre">
+               <div class="hijo">
+                    <img class="img-fluid"  src="../img/ruta_flores.jpg" alt="">   
+               </div>
+            </div>
         </div>
       </div>
       <div class="row GaleriaVias">
@@ -46,4 +53,17 @@
     </div>
   </section>
 </body>
+<style>
+    .hijo{
+        display: table-cell;
+        vertical-align: middle;
+    
+        
+    }
+    .padre{
+        text-align: center;
+        display: table;
+        height: 100%;
+    }
+</style>
 </html>
