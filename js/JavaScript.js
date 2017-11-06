63c7b1dbc81de6d1e855de24a98885eb7e97091e
Douglas_HdezT <00004516@uca.edu.sv>
2017-11-06 02:50:54 -0600

    cvxvccxc

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -79,4 +79,5 @@ function main () {
 			$('.modal-footer').css('filter','none');
 		});
 
+		
 }
---
+++ php/Vias_De_Comunicacion.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <?php
+    include '../html/head.html';
+  ?>
+  <title>Vias de Comunicacion</title>
+</head>
+<body>
+  <header>
+    <?php
+      include '../html/header.html';
+     ?>
+  </header>
+  <section>
+    <div class="container">
+      <h1 style="display: inline-block">Vias de comunicacion</h1><button type="button" class="play-buttom"><i name="play" class="fa fa-play" aria-hidden="true"></i><span name="play">Reproducir</span></button>
+      <div class="row" >
+        <div class="col-12">
+          <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+      <div class="row GaleriaVias">
+        <div class="col-lg-3 col-sm-6">
+          <img class="img-fluid" src="../img/Img5.jpg" alt="">
+        </div>
+        <div class="col-lg-3 col-sm-6 Limit">
+          <img class="img-fluid" src="../img/Img5.jpg" alt="">
+        </div>
+        <div class="col-lg-3 col-sm-6 Limit">
+          <img class="img-fluid" src="../img/Img5.jpg" alt="">
+        </div>
+        <div class="col-lg-3 col-sm-6 Limit">
+          <img class="img-fluid" src="../img/Img5.jpg" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
