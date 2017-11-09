85665e907008f1b512c57348fcd740b6922c8cf8
agmeag22 <00002816@uca.edu.sv>
2017-11-08 20:07:18 -0600

    081117
    Pesta;a de hidrografia

---
+++ php/hidrografia.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <?php
+    include '../html/head.html';
+    ?>
+    <title>Hidrografia - San Miguel de Mercedes</title>
+</head>
+<body>
+    <header>
+    <?php
+       include '../html/header.html';
+    ?>
+    </header>
+    <section>
+        <div class="container">
+
+            <h1 style="display: inline-block">Hidrografia 💧</h1>
+            <div class="row">
+              <div class="col-xs-12">
+               <a class="navbar-brand logo fadeIn animated" href="index.html">
+                  <img src="http://www.mapahidrografico.com/wp-content/uploads/2013/08/Mapa-Hidrografico-de-El-Salvador.gif" alt="mapa hidrografico" style="width: 100%;">
+               </a>
+             </div>
+          </div>
+          <div class="text-center" style="">
+       <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       <button type="button btn btn-large btn-success" class="play-buttom"><i name="play" class="fa fa-play" aria-hidden="true"></i>
+         <span name="play">Reproducir</span>
+     </button>
+     </div>
+          <div class="row">
+              <div class="col-lg-4 col-md-12 bounceInLeft animated">
+                  <a href="#rio1" class="thumbnail">
+
+                  </a>
+              </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-12 bounceInLeft animated">
+                    <a href="#" class="thumbnail">
+                      <img class="miniaruta" src="../img/Img8.jpg" alt="...">
+                    </a>
+                </div>
+                <div class="col-lg-8 col-md-12">
+                    <h1>Lorem</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p>
+                </div>
+            </div>
+            <div class="row">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt nisi facilis doloremque! Doloribus omnis, quaerat temporibus odio quo quasi voluptas dolorem. Sunt dolorum nostrum ab velit alias pariatur, libero porro?</p>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-lg-8 col-md-12">
+                    <h1>Lorem</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p>
+                </div>
+                <div class="col-lg-4 col-md-12 bounceInRight animated">
+                    <a href="#" class="thumbnail">
+                      <img class="miniaruta" src="../img/Img5.jpg" alt="...">
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-12 bounceInLeft animated">
+                    <a href="#rio1" class="thumbnail">
+
+                    </a>
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                  <div class="col-lg-4 col-md-12 bounceInLeft animated">
+                      <a href="#" class="thumbnail">
+                        <img class="miniaruta" src="../img/Img8.jpg" alt="...">
+                      </a>
+                  </div>
+                  <div class="col-lg-8 col-md-12">
+                      <h1>Lorem</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p>
+                  </div>
+              </div>
+              <div class="row">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt nisi facilis doloremque! Doloribus omnis, quaerat temporibus odio quo quasi voluptas dolorem. Sunt dolorum nostrum ab velit alias pariatur, libero porro?</p>
+              </div>
+              <hr>
+              <div class="row">
+                  <div class="col-lg-8 col-md-12">
+                      <h1>Lorem</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p>
+                  </div>
+                  <div class="col-lg-4 col-md-12 bounceInRight animated">
+                      <a href="#" class="thumbnail">
+                        <img class="miniaruta" src="../img/Img5.jpg" alt="...">
+                      </a>
+                  </div>
+              </div>
+          <hr>
+        <div class="row">
+            <div class="col-lg-4 col-md-12 bounceInLeft animated wowload">
+                <a href="#" class="thumbnail">
+                  <img class="miniaruta" src="../img/Img7.jpg" alt="...">
+                </a>
+            </div>
+            <div class="col-lg-8 col-md-12">
+                <h1>Lorem</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p>
+            </div>
+        </div>
+    </div>
+  </section>
+    <footer>
+        <?php
+          include '../html/Topscroller-dark.html';
+        ?>
+    </footer>
+    <!--Modals-->
+</body>
+</html>
