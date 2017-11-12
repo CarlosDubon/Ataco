55db72c67340753e45ca0f90c020a27b35a7742c
Kathgo97 <00011616@uca.edu.sv>
2017-11-12 11:54:47 -0600

    Actualizacion

--- php/orografia.php
+++ php/orografia.php
@@ -20,15 +20,36 @@
                     <h1><p align="center"> OROGRAFIA DE CONCEPCION DE ATACO</p></h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p> 
                 </div>
-                 <div class="col-lg-12 col-md-12">
+                 <div class="col-lg-10 col-md-12">
 
                     <img class="miniaruta" src="../img/relieve.jpg" alt="...">
                 </div>
-                 <div class="col-lg-12 col-md-12">
-               
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p> 
+                <div class="col-lg-2 col-md-12 ">
+                 <div class="col-md-12 polig1 col-sm-6">
+                    
                 </div>
-   
+
+                 <div class="col-md-12">
+                    Relieve Alto 
+                </div> <br><br> 
+
+                <div class="col-md-12 polig2 col-sm-6">
+                    
+                </div>
+
+                 <div class=" col-md-12 ">
+                    Relieve Mediano
+                </div> <br><br>
+
+                <div class="col-md-12 polig3 col-sm-6">
+                    
+                </div>
+
+                 <div class="col-md-12 ">
+                    Relieve Bajo
+                </div> <br><br>
+            </div>
+                
             </div>
         </div>
         
