b923bc2ff4004eba2c8ba03bf9b8c8be4c2c89f0
kilmarCabrera <kilmarhdz@gmail.com>
2017-11-12 21:37:55 -0600

    prueba

--- php/orografia.php
+++ php/orografia.php
@@ -18,7 +18,7 @@
               <div class="row">
                 <div class="col-lg-12 col-md-12">
                     <h1><p align="center"> OROGRAFIA DE CONCEPCION DE ATACO</p></h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequat                    sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos                    veritatis a quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p> 
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequat                    sunt doloremque esse accusamus dolor soluta excepturi minus, ipsam adipisci consectetur ad non, labore ab reiciendis id debitis quisquam hic mollitia! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere, natus necessitatibus dignissimos                    veritatis a         quisquam at minima fugiat dolor consectetur ullam adipisci, vitae labore eveniet tenetur quasi odio amet.</p> 
                 </div>
                  <div class="col-lg-10 col-md-12">
 
