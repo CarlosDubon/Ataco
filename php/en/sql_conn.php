9a683f3a138eb7e73c8f5f7316bdad6103fe283b
Douglas_HdezT <00004516@uca.edu.sv>
2017-11-18 16:05:13 -0600

    Cunm

--- php/en/sql_conn.php
+++ php/en/sql_conn.php
@@ -6,7 +6,7 @@
       $username = "root";
       $password = "";
     try{
-        $conn = new PDO('mysql:host=localhost;dbname=ataco;charset=utf8mb4', $username, $password);
+        $conn = new PDO('mysql:host=localhost;dbname=ataco_en;charset=utf8mb4', $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
       }catch(PDOException $e){
